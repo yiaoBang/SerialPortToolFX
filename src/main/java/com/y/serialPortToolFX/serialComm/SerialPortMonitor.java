98d0500dcc116967dfd70a57d2bb1397c7514c03
Y <1348719184@qq.com>
2024-04-04 13:09:28 +0800

    串口发送错误的时候关闭串口

--- src/main/java/com/y/serialPortToolFX/serialComm/SerialPortMonitor.java
+++ src/main/java/com/y/serialPortToolFX/serialComm/SerialPortMonitor.java
@@ -2,27 +2,16 @@ package com.y.serialPortToolFX.serialComm;
 
 import com.fazecast.jSerialComm.SerialPort;
 import com.y.serialPortToolFX.utils.FX;
-import javafx.beans.property.SimpleListProperty;
 import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.collections.FXCollections;
 
-import java.util.Arrays;
 import java.util.StringJoiner;
 
 public class SerialPortMonitor {
     public static volatile SerialPort[] commPorts = SerialPort.getCommPorts();
-    private static StringJoiner sj = new StringJoiner("\n");
     public static final SimpleStringProperty serialPorts = new SimpleStringProperty(serialPortsToString());
     public static final Thread serialPortMonitorThread;
 
     static {
-//        serialPorts.addListener((observable, oldValue, newValue) -> {
-//            System.out.println("=========================");
-//            System.out.println(newValue);
-//            System.out.println("=========================");
-//        });
         serialPortMonitorThread = new Thread(() -> {
             while (true) {
                 scanSerialPort();
@@ -42,12 +31,15 @@ public class SerialPortMonitor {
         SerialPort[] newPorts = SerialPort.getCommPorts();
         if (!checkSerialPort(newPorts)) {
             commPorts = newPorts;
-            FX.run(() -> serialPorts.set(serialPortsToString()));
-            sj = new StringJoiner("\n");
+            FX.run(() -> {
+                String s = serialPortsToString();
+                serialPorts.set(s == null ? "" : s);
+            });
         }
     }
 
     private static String serialPortsToString() {
+        StringJoiner sj = new StringJoiner("\n");
         for (SerialPort commPort : commPorts) {
             sj.add(commPort.getSystemPortName());
         }
